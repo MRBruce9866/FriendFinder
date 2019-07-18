2638d486fe91638f6160521730a07f04d04c61ff
Mark <mrbruce9866@gmail.com>
2019-07-17 22:01:27 -0400

    added a prototype method on User for getting the best match. Also set up the post resonse from the server

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,86 @@
+function User(obj){
+    this.name = obj.name;
+    this.photo = obj.photo;
+    this.scores = obj.scores;
 
-module.exports = [
+}
+
+User.prototype.getBestMatch = function (friends){
+    var match;
+    var matchDiff = 1000;
+    friends.forEach(friend => {
+        if(this !== friend){
+            var total = 0;
+           for (let i = 0; i < this.scores.length; i++) {
+               total += Math.abs(this.scores[i] - friend.scores[1]);
+           }
+
+           if(total < matchDiff){
+               match = friend;
+               matchDiff = total;
+           }
+        }
+    });
+
+    return match;
+}
+
+var friends = [
     {
-        "name":"Ahmed",
+        "name":"Test1",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
+        "scores":
+        [
             1,
-            4,
-            4,
-            5,
             1,
-            2,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            
+        ]
+    }, 
+    {
+        "name":"Test2",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":
+        [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+        ]
+    },
+    {
+        "name":"Test3",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":
+        [
             5,
-            4,
-            1
-          ]
-      }
-      
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+        ]
+    },    
 ]
+
+module.exports = {
+    friends,
+    User
+}
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,8 @@
 
 
 
-var friends = require('../data/friends.js');
+var friends = require('../data/friends.js').friends;
+var User = require('../data/friends.js').User;
 
 
 
@@ -12,9 +13,14 @@ app.get('/api/friends', function(req, res){
     res.json(friends);
 })
 
-// app.post('/api/friends', function(req, res){
-    
-// })
+app.post('/api/friends', function(req, res){
+    var newUser = new User(req.body);
+    var match = newUser.getBestMatch(friends);
+    friends.push(newUser);
+
+    res.json(match);
+
+})
 
 
 };

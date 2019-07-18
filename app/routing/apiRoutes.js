a69f077394b6d08e976810209ddb0f92fac7af06
Mark <mrbruce9866@gmail.com>
2019-07-17 22:52:54 -0400

    add post request to survey html and post response from the server

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,6 +14,11 @@ app.get('/api/friends', function(req, res){
 })
 
 app.post('/api/friends', function(req, res){
+
+    req.body.scores = req.body.scores.map(function (item){
+        return parseInt(item);
+    })
+
     var newUser = new User(req.body);
     var match = newUser.getBestMatch(friends);
     friends.push(newUser);
